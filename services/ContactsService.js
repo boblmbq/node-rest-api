912577346b578fe28d3b668ffd61c21dbc4349d1
Danylo_Sichkar <126655174+boblmbq@users.noreply.github.com>
2023-12-09 16:29:06 +0100

    start

---
+++ services/ContactsService.js
@@ -0,0 +1,48 @@
+const contacts = require("../models/contacts");
+const ContactModel = require("../models/contacts");
+
+class ContactsService {
+  constructor() {
+    this.ContactModel = ContactModel;
+  }
+
+  getContacts = async () => {
+    const contacts = await this.ContactModel.find({});
+    return contacts || null;
+  };
+
+  getContact = async (contactId) => {
+    const contact = await this.ContactModel.findById(contactId);
+    return contact || null;
+  };
+
+  createContact = async (body) => {
+    const createdContact = await this.ContactModel.create(body);
+    return createdContact || null;
+  };
+
+  deleteContact = async (contactId) => {
+    const deletedContact = await this.ContactModel.findByIdAndDelete(contactId);
+    return deletedContact || null;
+  };
+
+  updateContact = async (contactId, body) => {
+    const updatedContact = await this.ContactModel.findByIdAndUpdate(
+      contactId,
+      { ...body },
+      { new: true, runValidators: true }
+    );
+
+    return updatedContact || null;
+  };
+
+  updateStatusContact = async (contactId) => {
+    const updatedStatusContact = await this.ContactModel.findByIdAndUpdate(
+      contactId,
+      { ...body },
+      { new: true, runValidators: true }
+    );
+  };
+}
+
+module.exports = new ContactsService();
